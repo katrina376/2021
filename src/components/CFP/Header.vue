a94e53197757ca2147220a032919694ce1d79f64
hyperbola <hyperbola.cs07@gmail.com>
2021-01-15 19:50:16 +0800

    [Style] layout for CFP header

---
+++ src/components/CFP/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="sitcon-burnfont">
+    <div class="spot-box text-center">
+      <div class="sitcon-2021">
+        <img alt="SITCON2021" src="~@/assets/image/burnfont/2021.svg" />
+      </div>
+      <div class="sitcon-name">
+        <img alt="學生計算機年會" src="~@/assets/image/burnfont/sitcon.svg" />
+      </div>
+      <h1 class="sitcon-topic">
+        <img alt="算盤的後裔" src="~@/assets/image/burnfont/topic.svg" />
+      </h1>
+    </div>
+    <div class="info-box">
+      <div class="cfp"><img src="~@/assets/image/burnfont/cfp.svg" /></div>
+      <div class="date-location-wrapper">
+        <div class="date"><img src="~@/assets/image/burnfont/date.svg" /></div>
+        <div class="location">
+          <img src="~@/assets/image/burnfont/location.svg" />
+        </div>
+      </div>
+      <div class="info-wrapper">
+        <div class="hexagon">
+          <a rel="noopener" class="btn"
+            ><img src="~@/assets/image/burnfont/info.svg"
+          /></a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CfpHeader extends Vue {}
+</script>
--- src/pages/CFP.vue
+++ src/pages/CFP.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="cfp">
-    <h1>cfp</h1>
+    <CfpHeader id="cfp-header"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import CfpHeader from '@/components/CFP/Header.vue';
 
-@Component({})
+@Component({ components: { CfpHeader } })
 export default class CFP extends Vue {
   @Prop() private msg!: string;
 }
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/CFP/CFP.scss";
+</style>
+
