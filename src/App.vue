2ed74b6c732c36ec3c20515e24e87ddd806b30a3
secminhr <secminhrian@gmail.com>
2021-01-17 00:18:34 +0800

    [Fix] fix App.vue to make it run
    change Footer import path to match its name

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 // components
-import Footer from '@/components/Footer.vue';
+import Footer from '@/components/footer.vue';
 
 @Component({
   components: {
