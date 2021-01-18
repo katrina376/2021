7529769b18125d57645344da48ae78736be3b93a
Tony Yang <t510599@gmail.com>
2021-01-18 16:46:24 +0800

    [Add] Components: Topic.vue, AgendaCard.vue

---
+++ src/components/CFP/AgendaCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="agenda card">
+        <a class="image container" :href="ytlink" target="_blank" rel="noopener">
+            <img :src="img">
+        </a>
+        <a class="description" :href="introd" target="_blank" rel="noopener">{{ title }}</a>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AgendaCard extends Vue {
+    @Prop({ required: true }) private ytlink!: string;
+    @Prop({ required: true }) private introd!: string;
+    @Prop({ required: true }) private title!: string;
+    @Prop({ required: true }) private img!: string;
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/CFP/card.scss';
+</style>
---
+++ src/components/CFP/Topic.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="topic-example">
+        <h1 class="topic">{{ topic }}</h1>
+        <p class="description">{{ description }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Topic extends Vue {
+    @Prop({ required: true }) private topic!: string;
+    @Prop({ required: true }) private description!: string;
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/CFP/topic.scss';
+</style>
