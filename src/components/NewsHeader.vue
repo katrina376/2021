3639394d0a67cd4c973deb8a6ae40875c6aa35f3
hyperbola <hyperbola.cs07@gmail.com>
2021-01-18 18:57:51 +0800

    [Layout] mobile layout for News page

--- src/components/NewsHeader.vue
+++ src/components/NewsHeader.vue
@@ -9,14 +9,20 @@
       <div class="title-wrapper text-center">
         <img src="~@/assets/image/burnfont/news.svg" class="svg svg-cfp-news" />
       </div>
-      <div class="contribute-wrapper"><span>我要投稿</span></div>
+      <div class="contribute-wrapper">
+        <a class="btn-contribute" href="#">我要投稿</a>
+      </div>
     </div>
     <div class="cfp-body">
       <div class="spot-wrapper">
         <h1 class="text-center">現正徵稿中<span class="ignore">！</span></h1>
         <!-- TODO counter -->
         <p class="text-center countdown">
-          距離投稿截止還有 129 天 12 小時 12 分 12 秒
+          距離投稿截止還有
+          <span class="d-inline-block">
+            {{ countdown.d }} 天 {{ countdown.h }} 小時 {{ countdown.m }} 分
+            {{ countdown.s }} 秒</span
+          >
         </p>
       </div>
       <div class="time-wrapper">
@@ -32,7 +38,11 @@
 
         <div class="end-time-wrapper">
           <h2>投稿結束</h2>
-          <p>2021/02/22（一）日出<small>（清晨 06：23）</small></p>
+          <p>
+            2021/02/22（一）日出<small class="d-inline-block"
+              >（清晨 06：23）</small
+            >
+          </p>
         </div>
       </div>
     </div>
@@ -42,6 +52,65 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+const DEAD_LINE = Math.floor(
+  new Date('22 Feb 2021 06:24:00 GMT+8').getTime() / 1000
+);
+
+interface Countdown {
+  s: number;
+  m: number;
+  h: number;
+  d: number;
+}
+
 @Component
-export default class CfpHeader extends Vue {}
+export default class CfpHeader extends Vue {
+  private timerId!: number;
+  private countdown: Countdown = {
+    s: 0,
+    m: 0,
+    h: 0,
+    d: 0
+  };
+
+  public beforeMount () {
+    this.registerTimer();
+  }
+
+  public unmouted () {
+    this.unregisterTimer();
+  }
+
+  public async createTimer (f: (t: number) => void): Promise<number> {
+    const d = new Date();
+    let t = d.getTime();
+    const rem = 1000 - (t % 1000);
+
+    t = Math.floor(t / 1000);
+    let downcount = DEAD_LINE - t - 1;
+    this.onTick(downcount);
+    const id = setInterval(() => {
+      f(--downcount);
+    }, 1000);
+    return id;
+  }
+
+  private async onTick (t: number) {
+    this.countdown.s = t % 60;
+    t = Math.floor(t / 60);
+    this.countdown.m = t % 60;
+    t = Math.floor(t / 60);
+    this.countdown.h = t % 24;
+    t = Math.floor(t / 24);
+    this.countdown.d = t;
+  }
+
+  private async registerTimer () {
+    this.timerId = await this.createTimer(this.onTick);
+  }
+
+  private unregisterTimer () {
+    clearInterval(this.timerId);
+  }
+}
 </script>
